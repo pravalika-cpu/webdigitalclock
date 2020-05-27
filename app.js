4f348cc93ed7578f618e307a5c2be98a65c01214
pravalika-cpu <65751881+pravalika-cpu@users.noreply.github.com>
2020-05-27 17:39:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ var today=daysInWeek[day]
 var hours=date.getHours();
 var minutes=date.getMinutes();
 document.getElementById("dateAndTime").innerText=`${today} ${hours}:${minutes}`;
-
-
+var WeatherInCities=[   
+{city:"hyderabad,telangana",climaticCondition:"sunny",temperature:45},
+{city:"Bhopal,madhayapradesh",climaticcondition:"sunny",temperature:48},
+{city:"Imphal,manipur",climaticCondtion:"cloudy",temperature:15},
+{city:"Agartala,tripura",climaticCondition:"sunny",temperature:45},
+{city:"Bhopal,madhayapradesh",climaticcondition:"rainy",temperature:22},
+{city:"kohima,nagaland",climaticCondtion:"humid",temperature:29}
+]
 
 
