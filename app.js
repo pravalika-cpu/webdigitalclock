bd0e93a74b621f9952fa6e09a34a450ab7ae8355
pravalika-cpu <65751881+pravalika-cpu@users.noreply.github.com>
2020-05-27 19:00:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,11 +7,36 @@ var minutes=date.getMinutes();
 document.getElementById("dateAndTime").innerText=`${day} ${today} ${hours}:${minutes}`;
 var WeatherInCities=[   
 {city:"hyderabad,telangana",climaticCondition:"sunny",temperature:45},
-{city:"Bhopal,madhayapradesh",climaticcondition:"sunny",temperature:48},
+{city:"Bhopal,madhayapradesh",climaticcondition:"sunny",temperature:40},
 {city:"Imphal,manipur",climaticCondtion:"cloudy",temperature:15},
-{city:"Agartala,tripura",climaticCondition:"sunny",temperature:45},
-{city:"Bhopal,madhayapradesh",climaticcondition:"rainy",temperature:22},
-{city:"kohima,nagaland",climaticCondtion:"humid",temperature:29}
+{city:"kohima,nagaland",climaticCondition:"humid",temperature:22}
+{city:"agarthala,tripura",climaticCondtion:"humid",temperature:20}
 ]
 
 
+function getWeather(){
+    var list=document.getElementById("list")
+    var selectedCityFromList=list.options[list.selectedIndex].value;
+    var weatherInSelectedCity=weatherInCities.find(e=>{
+        return e.city==selectedCityFromList
+    })
+    document.getElementById("climate").innerText= weatherInSelectedCity.climateCondition;
+    document.getElementById("temp").innerText=weatherInSelectedCity.temperature;
+    document.getElementById("city").innerText=selectedCityFromList;
+}
+function getFahrenheit(){
+    var list=document.getElementById("list");
+    var selectedCityFromList=list.options[list.selectedIndex].value;
+    var weatherInSelectedCity=weatherInCities.find(e=>{
+        return e.city==selectedCityFromList
+})
+document.getElementById("temp").innerText=(weatherInSelectedCity.temperature*9/5)+32;
+}
+function getCelsius(){
+    var list=document.getElementById("list");
+    var selectedCityFromList=list.options[list.selectedIndex].value;
+    var weatherInSelectedCity=weatherInCities.find(e=>{
+        return e.city==selectedCityFromList
+})
+document.getElementById("temp").innerText=weatherInSelectedCity.temperature;
+}
